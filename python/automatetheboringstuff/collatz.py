ba1df1ad5975cb32885ebe4727a7becb69480e15
Andrei Nicolae <anicolae@localhost.localdomain>
2025-03-03 22:59:44 +0200

    This is the collatz sequence practice test from chapter 3.

---
+++ python/automatetheboringstuff/collatz.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        print(3 * number + 1)
+        return 3 * number + 1
+    else:
+        print('Please input a positive integer.')
+
+
+while True:
+    try:
+        print('Please enter a positive integer:')
+        number = input()
+        try:
+            collatz(int(number))
+        except ValueError:
+            print('You need to enter a positive integer.')
+    except KeyboardInterrupt:
+        sys.exit()                     
